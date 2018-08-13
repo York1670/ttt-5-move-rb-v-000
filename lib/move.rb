d20d145ec699161bde7570ccac0e10b4f680fd96
york1670@gmail.com <york1670@gmail.com>
2018-08-13 17:47:46 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ def input_to_index(user_input)
   user_input = user_input.to_i-1
 end
 
-def move(board, input_to_index, value = "X")
-  board[input_to_index] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
 
 def update_array_at_with(board, input_to_index, value = "X")
